f07597b6298ef3e8d7909207991c1f090b7afb36
devetea <108074530+devetea@users.noreply.github.com>
2022-06-27 20:47:45 +0900

    1. gradle  - 자바버전 수정  - mysql 드라이버 추가
    2. MySQLConnectionTEST.java
     - JDBC 연결 테스트

---
+++ reservation/src/test/java/com/movie/reservation/test/MySQLConnectionTEST.java
@@ -0,0 +1,33 @@
+package com.movie.reservation.test;
+
+import static org.mockito.ArgumentMatchers.contains;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.jupiter.api.Test;
+
+public class MySQLConnectionTEST {
+
+	
+	private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+	private static final String URL = "jdbc:mysql://localhost:3306/test?serverTimezone=UTC&allowPublicKeyRetrieval=true&useSSL=false";
+	private static final String USER = "root";
+	private static final String PASSWORD = "!#@Ty1096129";
+	
+	@Test
+	public void testConnection() throws Exception {
+		Class.forName(DRIVER);
+		
+		try {
+			System.out.println("######################### JDBC SUCCESS ############################");
+			Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+			System.out.println(connection);
+			System.out.println("###################################################################");
+		} catch (Exception e) {
+			System.out.println("########################## JDBC ERROR #################################");
+			System.out.println(e.getMessage());
+			System.out.println("#######################################################################");
+		}
+	}
+}
